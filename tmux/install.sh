b939ed866944e9181e06fd3542b0a6d5dc3145e0
nkone <phtruong@proton.me>
2023-06-28 10:18:49 -0700

    Update to wget for grabbing tmux.conf

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,4 +1,4 @@
 echo "Cloning tmux plugin manager tpm to ${HOME}/.tmux/plugins/tpm"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Copying tmux config to ${HOME}"
-cp .tmux.conf ~/.tmux.conf
+(cd ${HOME} && wget "https://raw.githubusercontent.com/nkone/nvim-basic-ide/master/tmux/.tmux.conf")
