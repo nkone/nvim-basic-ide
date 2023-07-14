1d23107e61c51dec55c9b799ab6fb11ac1f446c7
nkone <phtruong@proton.me>
2023-07-14 10:30:36 -0700

    Removed handlers and let noice to handle notifications

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -92,13 +92,13 @@ function M.config()
 
   vim.diagnostic.config(config)
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-  })
-
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-  })
+  -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  --   border = "rounded",
+  -- })
+  --
+  -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  --   border = "rounded",
+  -- })
 end
 
 return M
