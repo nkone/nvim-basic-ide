9fbad9f0422a73904822fb491dfc2dad8fa018ed
nkone <phtruong@proton.me>
2023-10-26 10:20:00 -0700

    Toggle Term update
    Added back esc mode when in terminal mode for horizontal and vertical splitting
    Changed default toggle to horizontal to enable multiple horizontal/vertical side by side
    Bound lazygit terminal and htop to float orientation

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -19,7 +19,7 @@ function M.config()
     start_in_insert = true,
     insert_mappings = true,
     persist_size = true,
-    direction = "float",
+    direction = "horizontal",
     close_on_exit = true,
     shell = vim.o.shell,
     float_opts = {
@@ -29,7 +29,7 @@ function M.config()
 
   function _G.set_terminal_keymaps()
     local opts = { noremap = true }
-    -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
@@ -39,13 +39,20 @@ function M.config()
   vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
 
   local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
+  local lazygit = Terminal:new {
+    cmd = "lazygit",
+    hidden = true,
+    direction = "float",
+    float_opts = {
+      border = "double",
+    },
+  }
 
   function _LAZYGIT_TOGGLE()
     lazygit:toggle()
   end
 
-  local htop = Terminal:new { cmd = "htop", hidden = true }
+  local htop = Terminal:new { cmd = "htop", hidden = true, direction = "float" }
   function _HTOP_TOGGLE()
     htop:toggle()
   end
